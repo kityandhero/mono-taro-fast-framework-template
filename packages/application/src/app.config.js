3a6a69f89bfb5e2f18998830bab57b2282583feb
kityandhero <kityandhero@126.com>
2025-03-25 17:55:12 +0800

    fix(taro-fast-application): :bug: update

--- packages/application/src/app.config.js
+++ packages/application/src/app.config.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-undef
 export default defineAppConfig({
+  entryPagePath: 'pages/entry/main/index',
   pages: [
     'pages/entry/main/index',
     'pages/home/main/index',
